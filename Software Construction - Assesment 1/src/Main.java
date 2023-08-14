f0350e49c266ad2641add3de3bbc827e76de11a2
Animeboynz <40583749+Animeboynz@users.noreply.github.com>
2023-08-15 11:59:20 +1200

    new changes

--- Software Construction - Assesment 1/src/Main.java
+++ Software Construction - Assesment 1/src/Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         Product p1 = new Product("9419788", "Product 1", 99.99);
-        Product p2 = new Product("1234567", "Product 2", 199.99);
+        Product p2 = new Product("1234dsadas567", "Product 2", 199.99);
         Product p3 = new Discount("9876543", "Product 3", 299.99, 40);
 
         CreateProduct c1;
